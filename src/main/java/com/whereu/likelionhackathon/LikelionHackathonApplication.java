fe402e47babbdc423b13633a714a5612a9acd022
mythpoy <okmlnsunok@gmail.com>
2023-07-31 23:10:38 +0100

    initial commit

---
+++ src/main/java/com/whereu/likelionhackathon/LikelionHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.whereu.likelionhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whereu/likelionhackathon/LikelionHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whereu.likelionhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
