6657595fd1f828e9d0193a403f4699c20cdc7399
mijin0721 <kgmi2010@naver.com>
2023-08-06 22:05:13 +0900

    feat : guide 한개 출력 기능 추가

--- src/main/java/com/whereu/likelionhackathon/domain/guide/controller/GuideController.java
+++ src/main/java/com/whereu/likelionhackathon/domain/guide/controller/GuideController.java
@@ -4,7 +4,9 @@ import com.whereu.likelionhackathon.domain.guide.dto.GuideDTO;
 import com.whereu.likelionhackathon.domain.guide.service.GuideService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class GuideController {
     public List<GuideDTO> guideFindAll() {
         log.info(guideService.findAll());
         return guideService.findAll();
+    }
+
+    @GetMapping("/{gid}")
+    public GuideDTO guidFindById(@PathVariable(name = "gid") Long gid) {
+        return guideService.findById(gid);
     }
 }
