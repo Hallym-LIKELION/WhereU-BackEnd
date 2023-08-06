4bf1b2358b6f152d237a5545e8d74af33902ac3f
mijin0721 <kgmi2010@naver.com>
2023-08-06 18:46:50 +0900

    test : 모든 내용을 보기 테스트 추가

---
+++ src/test/java/com/whereu/likelionhackathon/domain/user/repository/service/GuideServiceTest.java
@@ -0,0 +1,22 @@
+package com.whereu.likelionhackathon.domain.user.repository.service;
+
+
+import com.whereu.likelionhackathon.domain.guide.service.GuideService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("가이드에 관한 테스트")
+public class GuideServiceTest {
+    @Autowired
+    GuideService guideService;
+
+    @Test
+    @DisplayName("모든 내용 출력")
+    public void testFindAll() {
+        guideService.findAll();
+        System.out.println("모든 내용이 출력되었습니다.");
+    }
+}
