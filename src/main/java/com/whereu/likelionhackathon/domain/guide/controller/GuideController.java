b4ca3d47976d5ed4ef942139bede1d06b3e382db
mijin0721 <kgmi2010@naver.com>
2023-08-06 18:54:54 +0900

    feat : 가이드 전체조회 controller 추가

---
+++ src/main/java/com/whereu/likelionhackathon/domain/guide/controller/GuideController.java
@@ -0,0 +1,25 @@
+package com.whereu.likelionhackathon.domain.guide.controller;
+
+import com.whereu.likelionhackathon.domain.guide.dto.GuideDTO;
+import com.whereu.likelionhackathon.domain.guide.service.GuideService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/guide")
+@RequiredArgsConstructor
+@Log4j2
+public class GuideController {
+    private final GuideService guideService;
+
+    @GetMapping()
+    public List<GuideDTO> guideFindAll() {
+        log.info(guideService.findAll());
+        return guideService.findAll();
+    }
+}
