61bb4c137e58f5481bb374e2c22a4914436a993f
mijin0721 <kgmi2010@naver.com>
2023-08-09 18:18:05 +0900

    refactor : 가이드 db 저장 로직 수정

--- src/test/java/com/whereu/likelionhackathon/domain/user/repository/GuideRepositoryTest.java
+++ src/test/java/com/whereu/likelionhackathon/domain/user/repository/GuideRepositoryTest.java
@@ -17,27 +17,16 @@ public class GuideRepositoryTest {
     @DisplayName("가이드 내용 저장")
     public void guideSave() {
         String[][] list = {
-                {"침수", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent21.html?menuSeq=126"},
                 {"태풍", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent02.html?menuSeq=126"},
                 {"호우", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent18.html?menuSeq=126"},
-                {"낙뢰" ,"https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent03.html?menuSeq=126"},
                 {"강풍", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent04.html?menuSeq=126"},
                 {"풍랑", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent19.html?menuSeq=126"},
                 {"대설", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent05.html?menuSeq=126"},
                 {"한파", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent06.html?menuSeq=126"},
                 {"폭염", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent07.html?menuSeq=126"},
                 {"황사", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent08.html?menuSeq=126"},
-                {"지진", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent09.html?menuSeq=126"},
-                {"해일", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent10.html?menuSeq=126"},
-                {"지진해일", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent16.html?menuSeq=126"},
-                {"화산폭발", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent11.html?menuSeq=126"},
-                {"가뭄", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent12.html?menuSeq=126"},
-                {"홍수", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent13.html?menuSeq=126"},
-                {"해수면상승", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent17.html?menuSeq=126"},
-                {"산사태", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent20.html?menuSeq=126"},
-                {"자연우주물체추락", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent22.html?menuSeq=126"},
-                {"우주전파재난", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent23.html?menuSeq=126"},
-                {"조류대발생(녹조)", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent24.html?menuSeq=126"}
+                {"폭풍해일", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent10.html?menuSeq=126"},
+                {"건조, 산불", "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/SDIJKM5117.html?menuSeq=127"}
         };
 
         for (int i = 0; i < list.length; i++) {
