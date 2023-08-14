d3b73ad9be9b2e151fd106f6f53a8df91bc44d67
mythpoy <okmlnsunok@gmail.com>
2023-08-14 10:58:41 +0900

    Chore : choice의 유효성 검사 및 데이터 없을때 분기하여 반환

--- src/main/java/com/whereu/likelionhackathon/domain/weather/controller/WeatherController.java
+++ src/main/java/com/whereu/likelionhackathon/domain/weather/controller/WeatherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @Slf4j
@@ -19,8 +19,27 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping("{choice}")
-    public List<WeatherDTO> fetchWeatherDTO(@PathVariable(name = "choice") int choice) {
+    public Map<String, List<Object>> fetchWeatherDTO(@PathVariable(name = "choice") int choice) {
+        if (!choiceValidation(choice)) {
+            List<Object> errorMessage = List.of("유효하지 않은 특보 번호 입니다.");
+            return Map.of("result", errorMessage);
+        }
+
         List<WeatherDTO> weatherDTOList = weatherService.fetchData(choice);
-        return weatherDTOList;
+
+        if (weatherDTOList.size() == 0) {
+            List<Object> errorMessage = List.of("해당 특보 발령 지역이 없습니다.");
+            return Map.of("result", errorMessage);
+        } else {
+            return Map.of("result", new ArrayList<>(weatherDTOList));
+        }
+
     }
+
+    private boolean choiceValidation(int choice) {
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12};
+        Set<Integer> numberSet = new HashSet<>(Arrays.asList(numbers));
+        return numberSet.contains(choice) ? true : false;
+    }
+
 }
--- src/main/java/com/whereu/likelionhackathon/domain/weather/repository/WeatherRepository.java
+++ src/main/java/com/whereu/likelionhackathon/domain/weather/repository/WeatherRepository.java
@@ -2,13 +2,10 @@ package com.whereu.likelionhackathon.domain.weather.repository;
 
 import com.whereu.likelionhackathon.domain.weather.dto.LocationDTO;
 import com.whereu.likelionhackathon.domain.weather.entity.Weather;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
 
